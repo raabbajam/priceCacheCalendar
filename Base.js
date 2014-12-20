481419eab3877961b549a7b8f922ea7b05815ed4
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2014-12-20 20:12:37 +0700

    base@merge fix full on empty aoCheapest

--- Base.js
+++ Base.js
@@ -539,7 +539,7 @@ function merge(json) {
 	debug('aoCheapest', aoCheapest);
 	if (_.isEmpty(aoCheapest)) {
 		debug('Can\'t find some data. Return without cachePrices..');
-		return Promise.resolve(json);
+		return _this.mergeCachePrices(json);
 	}
 	return _this.getAllCachePrices(aoCheapest)
 		.catch(function(err) {
