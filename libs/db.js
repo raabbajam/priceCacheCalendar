84bb9725de1bc7bc87d40c9706474a6406dc2b4c
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2015-02-09 08:05:16 +0700

    change radio sriwijaya untuk mengatasi error ambil price

--- libs/db.js
+++ libs/db.js
@@ -7,9 +7,6 @@ if (process.env.CONFIG === 'local')
     config.host = 'localhost';
 if (!!process.env.SCRAPE_HOST)
     config.host = process.env.SCRAPE_HOST;
-
-config.host='pluto.live';
-
 var db = new ElasticSearchClient(config);
 // console.log('process.env.CONFIG %s', process.env.CONFIG);
 module.exports = db;
