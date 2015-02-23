df7673b1d5c64d97015af8d736983eda913b83d0
Agus Nurwanto Local <agusnurwantomuslim@gmail.com>
2015-02-23 14:36:46 +0700

    update get cheapest calendar

--- Base.js
+++ Base.js
@@ -250,7 +250,7 @@ function insertLowest(data) {
 			res = JSON.parse(res);
 			var oldPrice = (res._source && res._source.price) || 0;
 			debug(oldPrice, _price, data);
-			if(oldPrice==0 || (res._source && res._source.airline === _this.airline)){
+			if(oldPrice==0 || _price<oldPrice || (res._source && res._source.airline === _this.airline)){
 				data.price = _price;
 				debug('found lower price, inserting to calendar...', res);
 				_this.db.index('pluto', 'calendar', data, function(err, res) {
